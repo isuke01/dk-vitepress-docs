cf7285f7fccee6ee2bc03e1cf4633142d2df78e3
Łukasz ISU Biedroń <isuke.priv@gmail.com>
2025-05-07 17:32:24 +0200

    Update deeps

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,12 +1,9 @@
-import { SearchPlugin } from 'vitepress-plugin-search';
-import { defineConfig } from "vite";
-
-// @docs https://github.com/nextapps-de/flexsearch.
-const searchOpt = {
-	tokenize: "full",
-    resolution: 9
-}
+import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-	plugins: [SearchPlugin( searchOpt )],
+	themeConfig: {
+		search: {
+		  provider: 'local'
+		}
+	  }
 });
